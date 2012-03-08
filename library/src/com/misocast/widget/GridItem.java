935b99d4caf5df262126e9313ece3b6f22edf5ca
Nagoya0 <miso.pub@gmail.com>
2012-03-09 06:23:13 +0900

    Initial commit.

---
+++ library/src/com/misocast/widget/GridItem.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.misocast.widget;
+
+import android.graphics.Bitmap;
+
+/**
+ * @author Nagoya0
+ *
+ */
+public interface GridItem {
+
+	public Bitmap getBitmapInBackground(int pos);
+}
